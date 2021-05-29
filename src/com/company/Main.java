199571c32298ad3704537395ff2f94b70bc9acd3
Azhe <lollipopazhe@163.com>
2021-05-29 15:51:27 +0800

    6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(4);
         System.out.println(5);
+        System.out.println(6);
 	// write your code here
     }
 }
